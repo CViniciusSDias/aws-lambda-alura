1285ae155df40df5770e714c479880727533745d
Vinicius Dias <carlosv775@gmail.com>
2023-03-18 20:14:22 -0300

    Adicionando variável de ambiente no log

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import { log } from './log.mjs';
 
 export const handler = async(event) => {
-    log('Log de execução após GitHub Action. event: ' + JSON.stringify(event));
+    log('event: ' + JSON.stringify(event));
 
     return {
         statusCode: 200,
--- log.mjs
+++ log.mjs
@@ -1,3 +1,4 @@
 export function log(mensagem) {
-    console.log('Adicionando log via função', mensagem);
+    console.log(process.env.MINHA_VAR);
+    console.log({mensagem});
 }
