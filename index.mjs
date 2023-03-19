848a0034120a6f82b093f90e7a02ab18967c209c
Vinicius Dias <carlosv775@gmail.com>
2023-03-18 22:51:58 -0300

    Buscando objetos do S3

--- index.mjs
+++ index.mjs
@@ -1,13 +1,23 @@
 import { log } from './log.mjs';
+import { S3 } from '@aws-sdk/client-s3';
 
+const s3Client = new S3({ region: 'us-east-1' });
 export const handler = async(event) => {
-    log('event: ' + JSON.stringify(event));
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const Key = record.s3.object.key;
+    const getObjectResult = await s3Client.getObject({
+        Bucket,
+        Key,
+    });
+    const mega_byte = 1024 * 1024;
 
-    return {
-        statusCode: 200,
-        body: `<html><body>Dados da requisicao ${JSON.stringify(event)}</body></html>`,
-        headers: {
-            "content-type": "text/html"
-        }
-    };
+    if (getObjectResult.ContentLength > 1 * mega_byte) {
+        log('Objeto muito grande');
+
+        return 'Objeto muito grande';
+    }
+
+    log('Objeto de tamanho OK');
+    return 'Objeto de tamanho OK';
 };
